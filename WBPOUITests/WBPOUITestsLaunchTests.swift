811b511d15492b0ffc022dcac85f0b4de6b494c7
Marek Baláž <marek.balaz@casperise.com>
2023-04-27 18:35:08 +0200

    Initial Commit

---
+++ WBPOUITests/WBPOUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WBPOUITestsLaunchTests.swift
+//  WBPOUITests
+//
+//  Created by Marek Baláž on 27/04/2023.
+//
+
+import XCTest
+
+final class WBPOUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
